9fa3382c506ca2b7d68d1525cc0161593320afc8
bakit <eshmurzaaev_b@auca.kg>
2022-08-16 19:49:08 +0600

    Finish URI 1035

--- uri-midterm/src/URI1035.java
+++ uri-midterm/src/URI1035.java
@@ -6,6 +6,7 @@ public class URI1035 {
         int B = scanner.nextInt();
         int C = scanner.nextInt();
         int D = scanner.nextInt();
+
         if (B > C && D > A && ((C+D) > (A + B) && C > 0 && D > 0 && (A % 2 == 0))) {
             System.out.println("Valores aceitos");
         } else {
